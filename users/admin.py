8a9b22fc2563148fa86a89424b3f4f8e4c074cf2
Prince Kumar <princekumaraman7654@gmail.com>
2024-12-16 13:30:43 +0530

    register each app to admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import UserProfile, Wishlist, ReadingProgress, Bookmark
 # Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Wishlist)
+admin.site.register(ReadingProgress)
+admin.site.register(Bookmark)
