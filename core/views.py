b5c69f263f64fdc85d424b5aaa7ea8ede5c0d2c1
Prince Kumar <princekumaraman7654@gmail.com>
2024-12-17 20:04:40 +0530

    buid core app to manage homepage

--- core/views.py
+++ core/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 # Function to render home page
 def home_view(request):
-    return render(request, 'core/home.html') 
+    return render(request, 'core/base.html')
